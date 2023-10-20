3487dd0aa6100f8cb9f7912dd6ff557d88c83e28
St10092012 <St10092012@vcconnect.edu.za>
2023-10-20 13:54:59 +0200

    data test

--- pixel-pioneers-hackathon/src/routes/+page.server.ts
+++ pixel-pioneers-hackathon/src/routes/+page.server.ts
@@ -1,24 +1,26 @@
-import {PrismaClient} from '@prisma/client/edge'
-import type {PageServerLoad} from './$types';
+import { PrismaClient } from '@prisma/client/edge';
+import type { PageServerLoad } from './$types';
 
 let prisma: any = new PrismaClient();
 
 async function getRecord() {
-    return await prisma.user.findFirst();
+	return await prisma.user.findFirst();
 }
 
 export const load: PageServerLoad = async () => {
-    try {
-        const r = await getRecord();
-        return {"record": r, "error": null}; // Indicate success
-    } catch (e) {
-        // @ts-ignore
-        let err = {
-            "name": e?.name,
-            "message": e?.message,
-            "stack": e?.stack,
-        }
+	try {
+		const r = await getRecord();
+		return { record: r, error: null }; // Indicate success
+	} catch (e: any) {
+		// @ts-ignore
 
-        return {"record": null, "error": JSON.stringify(err)};
-    }
+		return {
+			record: null,
+			error: JSON.stringify({
+				name: e.name,
+				message: e.message,
+				stack: e.stack
+			})
+		};
+	}
 };
