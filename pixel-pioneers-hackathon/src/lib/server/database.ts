24afe817af630448e6e3accb2ad8888596fb88db
St10092012 <St10092012@vcconnect.edu.za>
2023-10-20 11:49:55 +0200

    add edge functions

--- pixel-pioneers-hackathon/src/lib/server/database.ts
+++
@@ -1,8 +0,0 @@
-const {PrismaClient} = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function getUsers() {
-    return await prisma.User.findMany();
-}
-
-module.exports = {getUsers};
--- pixel-pioneers-hackathon/src/routes/+page.server.ts
+++ pixel-pioneers-hackathon/src/routes/+page.server.ts
@@ -1,7 +1,11 @@
-import {PrismaClient} from '@prisma/client'
+// import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
 import type {PageServerLoad} from './$types';
+import {withAccelerate} from "@prisma/extension-accelerate";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends(withAccelerate())
+
 
 async function getRecord() {
     return await prisma.user.findFirst();
