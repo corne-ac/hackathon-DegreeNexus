2968459ba68a3a8584b4cb345a57a2f9df47a386
St10092012 <St10092012@vcconnect.edu.za>
2023-10-23 16:30:05 +0200

    Database testing and dark mode button added

--- pixel-pioneers-hackathon/src/routes/+page.server.ts
+++ pixel-pioneers-hackathon/src/routes/+page.server.ts
@@ -1,30 +1,29 @@
-import { PrismaClient } from '@prisma/client/edge';
 import type { PageServerLoad } from './$types';
 
 import { env } from '$env/dynamic/private';
+import { PrismaClient } from '@prisma/client/edge';
 
-let prisma: any = new PrismaClient({
+let prisma = new PrismaClient({
 	datasourceUrl: env.DATABASE_URL
 });
 
 async function getRecord() {
-	return await prisma.user.findFirst();
+	return await prisma.degree.findMany();
 }
 
 export const load: PageServerLoad = async () => {
 	try {
 		const r = await getRecord();
+
 		return { record: r, error: null }; // Indicate success
 	} catch (e: any) {
-		// @ts-ignore
-
 		return {
 			record: null,
-			error: JSON.stringify({
+			error: {
 				name: e.name,
 				message: e.message,
 				stack: e.stack
-			})
+			}
 		};
 	}
 };
