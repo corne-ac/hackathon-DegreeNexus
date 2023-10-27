7b59f4d5a5d6cd22b088ee2fff92b1a1c8c5fdf2
St10092012 <St10092012@vcconnect.edu.za>
2023-10-27 20:42:56 +0200

    test bcrypt

--- pixel-pioneers-hackathon/src/routes/auth/login/+page.server.ts
+++ pixel-pioneers-hackathon/src/routes/auth/login/+page.server.ts
@@ -1,8 +1,9 @@
 import type { Actions } from '@sveltejs/kit';
-import { RegisterUserSchema } from '$lib/validations/user.schema';
 import { env } from '$env/dynamic/private';
 import { PrismaClient } from '@prisma/client/edge';
+import * as bcrypt from 'bcrypt';
 
+const saltRounds = 10;
 let prisma = new PrismaClient({
 	datasourceUrl: env.DATABASE_URL
 });
@@ -12,18 +13,32 @@ export const actions: Actions = {
 	login: async ({ cookies, request }) => {
 		// Convert the form data into a JS object.
 		const formData = Object.fromEntries(await request.formData());
-		const validationResult = RegisterUserSchema.safeParse(formData);
+		// const validationResult = RegisterUserSchema.safeParse(formData);
 
 		// If the validation failed, return the errors and the form data.
-		if (!validationResult.success) {
-			console.log(validationResult.error);
-			let messages = validationResult.error.flatten().fieldErrors;
-			// We want to remove the current password values from the form data before returning it.
-			delete formData.password;
-			return { errors: messages, data: formData };
-		}
+		/* if (!validationResult.success) {
+       console.log(validationResult.error);
+       let messages = validationResult.error.flatten().fieldErrors;
+       // We want to remove the current password values from the form data before returning it.
+       delete formData.password;
+       return { errors: messages, data: formData };
+     }*/
 
-		let data = validationResult.data;
+		// let data = validationResult.data;
+
+		bcrypt.genSalt(saltRounds, (err, salt) => {
+			if (err) {
+				console.error('Error generating salt:', err);
+			} else {
+				bcrypt.hash(formData.password.toString(), salt, (err, hash) => {
+					if (err) {
+						console.error('Error hashing password:', err);
+					} else {
+						console.log('Hashed Password:', hash);
+					}
+				});
+			}
+		});
 
 		// Validation passed, add the user to db and get a token.
 	}
